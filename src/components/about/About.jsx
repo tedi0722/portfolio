cdbd510bc26930a10cb5013eef87725f5be0571a
tedi <tedi0722@users.noreply.github.com>
2018-12-14 18:16:16 -0800

    bio changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -22,7 +22,10 @@ export default class About extends Component {
                         <Image src={ ProfilePic } circle className="profile-pic" />
                     </Col>
                     <Col xs={12} md={8} className="bio-wrapper">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p>I'm a full-stack developer who wants to help a company grow through building and improving its applications and online presence. I enjoy decomposing and solving complicated problems and I’m always intrigued by the different possibilities and solutions that lies ahead. I strive to write clean codes and want to use my skills to ensure a business grows and remains on the cutting edge.</p>
+                        <p>When I'm not working, I love being outdoors, going on road trips, and shooting landscape photos.</p>
+                        <p>I am a hard worker and consider myself a lifelong learner. I’m confident that I’ll be able to contribute positively and I’m excited about what the future holds for my career.</p>
+                        <p>If you'd like to learn more about me, feel free to reach out on <Link to="/Contact">contact page</Link>.</p>
                     </Col>
                 </Row>
             </Grid>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,7 +10,7 @@ export default class Footer extends Component {
                 <a href="https://github.com/tedi0722" target="_blank" rel="noopener noreferrer">
                 <i className="fa fa-github-square" aria-hidden="true" />
                 </a>
-                <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/ted-i-8a9813131/" target="_blank" rel="noopener noreferrer">
                 <i className="fa fa-linkedin-square" aria-hidden="true" />
                 </a>
                 <a href="https://www.instagram.com/tsi.jpg/" target="_blank" rel="noopener noreferrer">
